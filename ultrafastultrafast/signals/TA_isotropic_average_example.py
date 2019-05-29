867d661032a11aa40a331eba6a48e73130635420
Peter Rose <peter.rose56@gmail.com>
2019-05-29 10:57:27 -0400

    Added comments to TA_isotropic_average_example.py

--- ultrafastultrafast/signals/TA_isotropic_average_example.py
+++ ultrafastultrafast/signals/TA_isotropic_average_example.py
@@ -1,5 +1,9 @@
+#Standard python libraries
+import os
+
 #Dependencies
 import numpy as np
+import yaml
 import matplotlib.pyplot as plt
 from pyfftw.interfaces.numpy_fft import fft, fftshift, ifft, ifftshift, fftfreq
 
@@ -14,15 +18,18 @@ Electrodynamics, by Akbar Salam
 I4_mat = np.array([[4,-1,-1],[-1,4,-1],[-1,-1,4]])/30
 
 def kdel(x,y):
+    """Kronecker Delta"""
     if x == y:
         return 1
     else:
         return 0
 
 def kdel2(a,b,c,d):
+    """Product of 2 Kronecker Deltas"""
     return kdel(a,b)*kdel(c,d)
 
 def kdelvec(i,j,k,l):
+    """Length 3 vector of Kronecker Delta products, as defined in """
     vec = [kdel2(i,j,k,l),
            kdel2(i,k,j,l),
            kdel2(i,l,j,k)]
@@ -30,45 +37,60 @@ def kdelvec(i,j,k,l):
 
 class TransientAbsorptionIsotropicAverage(object):
     """This class performs the isotropic average of the 4th order tensor
-which is the material response produced by 4-wave mixing process"""
+        which is the material response produced by 4-wave mixing process"""
     def __init__(self,parameter_file_path,efield_polarization,*, num_conv_points=138,
                  initial_state=0,dt=0.1,total_num_time_points = 3686):
+        # This is the object that will actually calculate all of the signals
         self.TA = TransientAbsorption(parameter_file_path,
                                       num_conv_points=num_conv_points,
                                       initial_state=initial_state, dt=dt,
                                       total_num_time_points=total_num_time_points)
-
+        # Lab frame polarization sequence for pulses
         self.efield_polarization = efield_polarization
+        # Working directory
         self.base_path = self.TA.base_path
 
-    def set_pulse_shapes(self,*args):
-        self.TA.set_pulse_shapes(*args)
+    def set_pulse_shapes(self,*args,**kwargs):
+        # Pass pulse shapes on to the self.TA object
+        self.TA.set_pulse_shapes(*args,**kwargs)
 
     def calculate_spectra(self,delay_times):
+        # The isotropic averaging is performed based upon the real orientation of the lab frame pulses
+        # The calculation will differ depending on whether the pump and probe are aligned or crossed
         left_vec = kdelvec(*self.efield_polarization)
 
         xyz = ['x','y','z']
 
         pol_options = []
         for i in range(3):
+            # Check to see if the dipole operator has any non-zero components along the given
+            # molecular frame axis, if the dipole exists only in the x-y plane, for example,
+            # then we can avoid doing quite a few unnecessary calculations!
             if not np.allclose(self.TA.mu_GSM_to_SEM[:,:,i],0):
                 pol_options.append(xyz[i])
-
+        
         signal = np.zeros((self.TA.w.size,delay_times.size))
 
         for i in pol_options:
             for j in pol_options:
                 for k in pol_options:
                     for l in pol_options:
+                        # generate the vector of kronecker delta products
                         right_vec = kdelvec(i,j,k,l)
                         if np.allclose(right_vec,0):
+                            # If the vector is 0, don't bother!
                             pass
                         else:
+                            # If not, set the polarization sequence, do the calculation, and
+                            # add the weight given by the isotropic weight matrix, I4_mat
+                            # Note the the polarization sequences are not the lab frame
+                            # polarization sequence of the pulses.
                             self.TA.set_polarization_sequence([i,j,k,l])
                             weight = I4_mat.dot(right_vec)
                             weight = np.dot(left_vec,weight)
                             signal += weight * self.TA.calculate_pump_probe_spectra_vs_delay_time(delay_times)
-
+                            
+        # Full isotorpically averaged signal
         self.signal_vs_delay_times = signal
         self.delay_times = delay_times
         self.w = self.TA.w
